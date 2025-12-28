68cfe42d8107da64f49e2840ac6fc0e99921add9
Nitin <jinagamnitin456@gmail.com>
2025-12-28 12:35:51 +0530

    Modified Java-service to use springboot

---
+++ Java-service/src/test/java/com/example/ragbot/RagbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ragbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
